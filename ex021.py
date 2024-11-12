659409ba9151c40128d5b5b521bafa38bb33dde4
Müller <mrmiller94@gmail.com>
2024-11-12 02:39:04 -0300

    Corrigindo nome da váriavel do input de parar a música.

--- ex021.py
+++ ex021.py
@@ -5,4 +5,4 @@ mixer.init() #inicia a função de reprodução de áudio
 mixer.music.load(r'C:\Users\Guilherme\Downloads\thinking-time-ticking-power-223023.mp3') #carrega a música da máquina, o R é para tratar o texto como ele aparece, sem interpretar como outra coisa
 mixer.music.play() #inicia a música
 
-x = input('Digite algo para parar a reprodução: ') #digitar qualquer coisa + enter pra parar a reprodução
+stop = input('Digite algo para parar a reprodução: ') #digitar qualquer coisa + enter pra parar a reprodução
