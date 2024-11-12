58a95e88e382e3e214f7a050aead686bdd49188b
Müller <mrmiller94@gmail.com>
2024-11-12 02:45:33 -0300

    Colocando a música dentro da pasta do projeto

--- ex021.py
+++ ex021.py
@@ -2,7 +2,7 @@ from pygame import mixer #importa o mixer da lib pygame
 
 mixer.init() #inicia a função de reprodução de áudio
 
-mixer.music.load(r'C:\Users\Guilherme\Downloads\thinking-time-ticking-power-223023.mp3') #carrega a música da máquina, o R é para tratar o texto como ele aparece, sem interpretar como outra coisa
+mixer.music.load('audio.mp3') #seleciona a música dentro da pasta do projeto
 mixer.music.play() #inicia a música
 
 stop = input('Digite algo para parar a reprodução: ') #digitar qualquer coisa + enter pra parar a reprodução
