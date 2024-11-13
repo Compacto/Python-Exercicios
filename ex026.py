0535683f9c0e06c6df9dc6a2f8c891b72c12f8b8
Müller <mrmiller94@gmail.com>
2024-11-13 01:49:18 -0300

    Algumas alterações irrelevantes

--- ex026.py
+++ ex026.py
@@ -1,6 +1,6 @@
-frase = str(input('Digite uma frase: '))
+frase = str(input('Digite uma frase: ')).upper().strip()
 
 print(f'Dados da frase:\n'
-      f'A letra "A" aparece {frase.count("a", 0)} vezes na frase\n'
-      f'A letra "A" aparece pela primeira vez na posição {frase.lower().find("a")}\n'
-      f'A letra "A" aparece pela última vez na posição: {frase.lower().rfind("a")}')
+      f'A letra "A" aparece {frase.count("A", 0)} vezes na frase\n'
+      f'A letra "A" aparece pela primeira vez na posição {frase.lower().find("a")+1}\n'
+      f'A letra "A" aparece pela última vez na posição: {frase.lower().rfind("a")+1}')
